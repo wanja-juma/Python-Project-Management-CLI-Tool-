46e9331270ddea864060d74f7bae27bf4689f5c0
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-06-15 17:13:10 +0300

    implemented models file

---
+++ models/person.py
@@ -0,0 +1,20 @@
+class Person:
+    """Base class for all people."""
+
+    def __init__(self, name, email):
+        self._name = name
+        self._email = email
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if "@" not in value:
+            raise ValueError("Invalid email")
+        self._email = value
